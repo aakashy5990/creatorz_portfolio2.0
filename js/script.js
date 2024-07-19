e556c3c481118443e64dd5ce3cd311d397c20e1d
Aakash yadav <126560106+aakashy5990@users.noreply.github.com>
2024-07-19 22:27:03 +0530

    Done

--- js/script.js
+++ js/script.js
@@ -72,3 +72,29 @@ document.addEventListener('DOMContentLoaded', function() {
     // Call incrementCounts function every initialIncrementSpeed milliseconds
     const intervalId = setInterval(incrementCounts, initialIncrementSpeed);
 });
+
+
+
+
+
+// cursor js code 
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove",function (e) {
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    // cursorOutline.style.left = `${posX}px`;
+    // cursorOutline.style.top = `${posY}px`;
+    
+    cursorOutline.animate({
+        left: `${posX}px`,
+        top:`${posY}px`
+    },{ duration:1000, fill:"forwards"});
+
+});
+
